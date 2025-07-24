968af5fed998b197f5740df2b625ebd90596dcc0
kiriasru <kiriasru@gmail.com>
2025-07-23 19:25:25 -0600

    Adding data

--- Pedidos/Program.cs
+++ Pedidos/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Pedidos;
+// Objetos
+Producto producto = new Producto();
+producto.Id = 1;
+producto.Nombre = "Laptop";
+producto.Precio = 5000;
+producto.Stock = 10;
+
+Producto producto1 = new Producto();
+producto1.Id = 2;
+producto1.Nombre = "Mouse";
+producto1.Precio = 250;
+producto1.Stock = 15;
+
+// Mostrar productos
+producto.MostrarInformacion();
+producto1.MostrarInformacion();
+
+// Reducir del stock y mostrar
+producto.DisminuirStock(1);
+producto.MostrarInformacion();
+
+producto1.DisminuirStock(4);
+producto1.MostrarInformacion();
