b8ef9babceac0050f1ad09fa5b0ec039bc63e704
kiriasru <kiriasru@gmail.com>
2025-07-25 19:27:47 -0600

    Update Producto.cs
    Adding price Format

--- Pedidos/Producto.cs
+++ Pedidos/Producto.cs
@@ -3,26 +3,22 @@ namespace Pedidos
 {
     public class Producto
     {
-        // Atributos
         public int Id { get; set; }
         public string Nombre { get; set; }
         public decimal Precio { get; set; }
-        public int Stock {  get; set; }
+        public int Stock { get; set; }
 
-        // Metodos
         public void MostrarInformacion()
         {
             Console.WriteLine("***** DATOS DEL PRODUCTO *****");
             Console.WriteLine($"ID: {Id}");
             Console.WriteLine($"Nombre: {Nombre}");
-            Console.WriteLine($"Precio: {Precio}");
+            Console.WriteLine($"Precio: {Precio:C}");
             Console.WriteLine($"Cantidad disponible: {Stock}");
             Console.WriteLine();
         }
 
-        // Si el constructor no lleva params, C# automaticamente los crea por detras
-       
-        // Terminar el constructor
+        // Si el constructor no lleva parámetros, C# automáticamente lo crea por detrás
 
         public void DisminuirStock(int Cantidad)
         {
@@ -33,7 +29,8 @@ namespace Pedidos
             else
             {
                 Console.WriteLine($"No hay suficiente stock de {Nombre}");
+                Console.WriteLine();
             }
         }
-    } // Fin de Class
-} // Fin de Pedidos
+    }
+}
