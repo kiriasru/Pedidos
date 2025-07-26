676bfe5dedc749bcd117c55411430b4e94f3ec3a
kiriasru <kiriasru@gmail.com>
2025-07-25 19:27:22 -0600

    Create ItemPedido.cs
    Create Item Pedido Class

---
+++ Pedidos/ItemPedido.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Pedidos
+{
+    public class ItemPedido
+    {
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Subtotal => Producto.Precio * Cantidad;
+
+        public ItemPedido(Producto Producto, int Cantidad) 
+        {
+            this.Producto = Producto;
+            this.Cantidad = Cantidad;
+        }
+    }
+}
