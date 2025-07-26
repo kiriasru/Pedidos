926be4bf3655f49fdf6d1f6efc762ee662c2cc85
kiriasru <kiriasru@gmail.com>
2025-07-25 19:27:52 -0600

    Update Program.cs
    Adding data

--- Pedidos/Program.cs
+++ Pedidos/Program.cs
@@ -1,24 +1,26 @@
-﻿using Pedidos;
-// Objetos
+﻿
+using Pedidos;
+
 Producto producto = new Producto();
 producto.Id = 1;
 producto.Nombre = "Laptop";
 producto.Precio = 5000;
 producto.Stock = 10;
 
-Producto producto1 = new Producto();
+producto.MostrarInformacion();
+
+Producto producto1 = new Producto(); 
 producto1.Id = 2;
 producto1.Nombre = "Mouse";
 producto1.Precio = 250;
 producto1.Stock = 15;
 
-// Mostrar productos
-producto.MostrarInformacion();
 producto1.MostrarInformacion();
 
-// Reducir del stock y mostrar
-producto.DisminuirStock(1);
-producto.MostrarInformacion();
+Pedido pedido = new Pedido(1);
+pedido.AgregarItem(producto, 1);
+pedido.AgregarItem(producto1, 5);
+pedido.MostrarDetalles();
 
-producto1.DisminuirStock(4);
+producto.MostrarInformacion();
 producto1.MostrarInformacion();
