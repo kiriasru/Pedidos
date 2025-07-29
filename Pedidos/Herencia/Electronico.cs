1f729914cc7df12e460f2ef24e0c9c9f78607078
kiriasru <kiriasru@gmail.com>
2025-07-28 19:22:56 -0600

    Create derived class

---
+++ Pedidos/Herencia/Electronico.cs
@@ -0,0 +1,14 @@
+﻿namespace Pedidos.Herencia;
+
+public class Electronico : Producto
+{
+    public int GarantiaMeses { get; set; }
+    public string Voltaje { get; set; }
+
+    public override void MostrarInformacion()
+    {
+        base.MostrarInformacion();
+        Console.WriteLine($"Garantia: {GarantiaMeses} meses");
+        Console.WriteLine($"Voltaje: {Voltaje}");
+    }
+}
