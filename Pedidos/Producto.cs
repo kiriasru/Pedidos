318870eac4b56c17e8fb8c5039650e270342a1e3
kiriasru <kiriasru@gmail.com>
2025-07-28 19:23:25 -0600

    Adding virtual syntax to override in derived classes

--- Pedidos/Producto.cs
+++ Pedidos/Producto.cs
@@ -8,14 +8,14 @@ namespace Pedidos
         public decimal Precio { get; set; }
         public int Stock { get; set; }
 
-        public void MostrarInformacion()
+        public virtual void MostrarInformacion()
         {
+            Console.WriteLine();
             Console.WriteLine("***** DATOS DEL PRODUCTO *****");
             Console.WriteLine($"ID: {Id}");
             Console.WriteLine($"Nombre: {Nombre}");
             Console.WriteLine($"Precio: {Precio:C}");
             Console.WriteLine($"Cantidad disponible: {Stock}");
-            Console.WriteLine();
         }
 
         // Si el constructor no lleva parámetros, C# automáticamente lo crea por detrás
