123f020321b3ecb77cbedcf0f4011476aad2886a
kiriasru <kiriasru@gmail.com>
2025-07-30 18:39:11 -0600

    Adding Derived Classs

---
+++ Pedidos/Herencia/Libro.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Channels;
+
+namespace Pedidos.Herencia;
+
+public class Libro : Producto
+{
+    // Atributos
+    public string ISBN { get; set; }
+    public string Autor { get; set; }
+    public int NumeroPaginas { get; set; }
+
+    public override void MostrarInformacion()
+    {
+        base.MostrarInformacion();
+        Console.WriteLine($"ISBN: {ISBN}");
+        Console.WriteLine($"Autor: {Autor}");
+        Console.WriteLine($"Número de Páginas: {NumeroPaginas}");
+    } // Fin de MostrarInformacion
+} // Fin de class
