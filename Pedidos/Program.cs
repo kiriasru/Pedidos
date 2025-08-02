f6b0110c69062c79a970e3819ced66a99823b731
kiriasru <kiriasru@gmail.com>
2025-08-01 19:01:17 -0600

    Adding data

--- Pedidos/Program.cs
+++ Pedidos/Program.cs
@@ -2,33 +2,6 @@
 using Pedidos;
 using Pedidos.Herencia;
 
-// Creando objeto producto
-Producto producto = new Producto();
-producto.Id = 1;
-producto.Nombre = "Laptop";
-producto.Precio = 5000;
-producto.Stock = 10;
-
-producto.MostrarInformacion();
-
-// Creando objeto producto1
-Producto producto1 = new Producto(); 
-producto1.Id = 2;
-producto1.Nombre = "Mouse";
-producto1.Precio = 250;
-producto1.Stock = 15;
-
-producto1.MostrarInformacion();
-
-// Usando metodos de pedido
-Pedido pedido = new Pedido(1);
-pedido.AgregarItem(producto, 1);
-pedido.AgregarItem(producto1, 5);
-pedido.MostrarDetalles();
-
-producto.MostrarInformacion();
-producto1.MostrarInformacion();
-
 // Creando objeto electronico
 Electronico electronico = new Electronico();
 electronico.Id = 3;
@@ -38,6 +11,15 @@ electronico.Stock = 200;
 electronico.GarantiaMeses = 12;
 electronico.Voltaje = "110V";
 
+electronico.MostrarInformacion();
+
+// Usando metodos de producto (heredados)
+Pedido pedido = new Pedido(1);
+pedido.AgregarItem(electronico, 1);
+pedido.AgregarItem(electronico, 5);
+pedido.MostrarDetalles();
+
+electronico.MostrarInformacion();
 electronico.MostrarInformacion();
 
 // Crear libro
@@ -52,6 +34,14 @@ libro.NumeroPaginas = 500;
 
 libro.MostrarInformacion();
 
+Pedido pedido1 = new Pedido(2);
+pedido1.AgregarItem(libro, 10);
+pedido1.AgregarItem(libro, 22);
+pedido1.MostrarDetalles();
+
+libro.MostrarInformacion();
+libro.MostrarInformacion();
+
 // Crear libro 1
 Libro libro1 = new Libro();
 libro1.Id = 5;
@@ -62,4 +52,12 @@ libro1.ISBN = "6070731743";
 libro1.Autor = "Dross";
 libro1.NumeroPaginas = 471;
 
+libro1.MostrarInformacion();
+
+Pedido pedido2 = new Pedido(3);
+pedido2.AgregarItem(libro1, 15);
+pedido2.AgregarItem(libro1, 25);
+pedido2.MostrarDetalles();
+
+libro1.MostrarInformacion();
 libro1.MostrarInformacion();
